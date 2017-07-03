ec01fd3115f3033831314e1eac405b4b206abc15
とろゝ <toroponz@outlook.com>
2017-07-03 22:04:18 +0900

    ツイートトレイの初期値を変更

--- TweetGazer/Properties/Settings.Designer.cs
+++ TweetGazer/Properties/Settings.Designer.cs
@@ -193,7 +193,7 @@ namespace TweetGazer.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
         public bool IsVisibleCreateStatusTray {
             get {
                 return ((bool)(this["IsVisibleCreateStatusTray"]));
