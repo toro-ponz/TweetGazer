fd8568f191a92e534a52a923d6f5663eb1aa16ce
とろゝ <toro-ponz@users.noreply.github.com>
2018-03-23 05:25:46 +0900

    AddTimelineTrendGridのVisibility削除

---
+++ TweetGazer/Views/MainWindows/Flyouts/ExtraGrid/Trends.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace TweetGazer.Views.MainWindows.Flyouts.ExtraGrid
+{
+    /// <summary>
+    /// Trends.xaml の相互作用ロジック
+    /// </summary>
+    public partial class Trends : UserControl
+    {
+        public Trends()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
